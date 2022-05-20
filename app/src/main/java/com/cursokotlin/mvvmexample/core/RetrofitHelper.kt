9449bad1983e82fb2079531c8e1ac508418821c0
Anton509XD <antoniovelazco543@gmail.com>
2022-05-19 22:46:12 -0500

    url de base de datos

--- app/src/main/java/com/cursokotlin/mvvmexample/core/RetrofitHelper.kt
+++ app/src/main/java/com/cursokotlin/mvvmexample/core/RetrofitHelper.kt
@@ -2,7 +2,7 @@ package com.cursokotlin.mvvmexample.core
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
+//url de la base de datos
 object RetrofitHelper {
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
